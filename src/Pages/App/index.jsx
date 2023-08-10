306fd5eb543245806da70d7a45d13352695f4cef
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-10 16:42:10 -0300

    Add routing

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,24 +1,30 @@
-
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
-
 import './App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sing-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
+  ])
 
+  return routes
+}
+
+const App = () => {
   return (
-    <div className='bg-teal-700'>
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <NotFound />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
