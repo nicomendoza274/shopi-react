0d52d8774231fa67947f6e7e1a8525ba26bb154c
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-11 15:51:43 -0300

    consume API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,16 +1,17 @@
-const Card = () => {
+const Card = (data) => {
+  const record = data.data
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
       <figure className='relative mb-2 w-full h-4/5'>
-        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics</span>
-        <img className='w-full h-full object-cover rounded-lg' src='https://randomfox.ca/images/39.jpg' alt='headphones' />
+        <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{record.category?.name}</span>
+        <img className='w-full h-full object-cover rounded-lg' src={record.images[0]} alt={record.title} />
         <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'>
           +
         </div>
       </figure>
-      <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+      <p className="flex justify-between items-center">
+        <span className="text-sm font-light truncate mr-2">{record.title}</span>
+        <span className="text-lg font-medium">${record.price}</span>
       </p>
     </div>
   )
