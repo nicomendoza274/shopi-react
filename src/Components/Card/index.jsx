68169dfac71fcf9feb60e58100e4eddf14322c97
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-12 21:14:20 -0300

    Open & close Product Detail

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,16 +5,20 @@ import { ShoppingCartContext } from '../../Context'
 const Card = (data) => {
   const record = data.data
   const context = useContext(ShoppingCartContext)
-  const onclick = () => context.setCount(context.count + 1)
-
+  const addCount = () => context.setCount(context.count + 1)
+  const openDetail = () => context.openProductDetail()
+  
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+    <div 
+      className='bg-white cursor-pointer w-56 h-60 rounded-lg'
+      onClick={openDetail}
+    >
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{record.category?.name}</span>
         <img className='w-full h-full object-cover rounded-lg' src={record.images[0]} alt={record.title} />
         <div 
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={onclick}
+          onClick={addCount}
         >
           <PlusIcon className='h-6 w-6 text-black'/>
         </div>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,19 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
 import './styles.css'
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext)
+  const isProductDetailOpen = context.isProductDetailOpen
+  const closeProductDetail = () => context.closeProductDetail()
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded bg-white'>
+    <aside 
+      className={`${isProductDetailOpen ? 'flex' : 'hidden'} product-detail  flex-col fixed right-0 border border-black rounded bg-white`}
+    >
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
-        <div>
+        <div onClick={closeProductDetail}>
           <XMarkIcon className='h-6 w-6 text-black'/>
         </div>
       </div>
