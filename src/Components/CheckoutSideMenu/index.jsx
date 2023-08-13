dea30fac15e2ca5f5a72cc88375f580f6dcc5a4b
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-12 23:08:38 -0300

    Add Order Card component

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,12 +1,14 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
   const isCheckoutSideMenuOpen = context.isCheckoutSideMenuOpen
   const closeCheckoutSideMenu = () => context.closeCheckoutSideMenu()
+  console.log('CART', context.cartProducts)
 
   return (
     <aside 
@@ -15,10 +17,21 @@ const CheckoutSideMenu = () => {
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>My Order</h2>
         <div className='cursor-pointer' onClick={() => closeCheckoutSideMenu()}>
-          <XMarkIcon className='h-6 w-6 text-black'/>
+          <XMarkIcon className='h-6 w-6 text-black cursor-pointer'/>
         </div>
       </div>
-
+      <div className='px-6'>
+        {
+          context.cartProducts.map((product) =>(
+            <OrderCard
+              key={product.id}
+              title={product.title} 
+              imageUrl={product.images}
+              price={product.price}
+            />
+          ))
+        }
+      </div>
     </aside>
   )
 }
