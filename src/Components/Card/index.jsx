d3c63ac57574ad01ecda3227ca4dafbae1e91579
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-12 21:56:37 -0300

    show product details

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -7,15 +7,19 @@ const Card = (data) => {
   const context = useContext(ShoppingCartContext)
   const addCount = () => context.setCount(context.count + 1)
   const openDetail = () => context.openProductDetail()
+  const showProduct = (productDetail) => {
+    context.setProductToShow(productDetail)
+    openDetail()
+  }
   
   return (
     <div 
       className='bg-white cursor-pointer w-56 h-60 rounded-lg'
-      onClick={openDetail}
+      onClick={() => showProduct(record)}
     >
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{record.category?.name}</span>
-        <img className='w-full h-full object-cover rounded-lg' src={record.images[0]} alt={record.title} />
+        <img className='w-full h-full object-cover object-top rounded-lg' src={record.images[0]} alt={record.title} />
         <div 
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
           onClick={addCount}
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -7,16 +7,30 @@ const ProductDetail = () => {
   const context = useContext(ShoppingCartContext)
   const isProductDetailOpen = context.isProductDetailOpen
   const closeProductDetail = () => context.closeProductDetail()
+  const productToShow = context.productToShow
+
   return (
     <aside 
       className={`${isProductDetailOpen ? 'flex' : 'hidden'} product-detail  flex-col fixed right-0 border border-black rounded bg-white`}
     >
       <div className='flex justify-between items-center p-6'>
         <h2 className='font-medium text-xl'>Detail</h2>
-        <div onClick={closeProductDetail}>
+        <div className='cursor-pointer' onClick={closeProductDetail}>
           <XMarkIcon className='h-6 w-6 text-black'/>
         </div>
       </div>
+      <figure className='px-6'>
+        <img 
+          className='w-full h-full rounded-lg' 
+          src={productToShow?.images ? productToShow?.images[0] : ''} 
+          alt={productToShow.title} 
+        />
+      </figure>
+      <p className='flex flex-col p-6'>
+        <span className='font-medium text-2xl mb-2'>${productToShow.price}</span>
+        <span className='font-medium text-md'>{productToShow.title}</span>
+        <span className='font-light text-sm'>{productToShow.description}</span>
+      </p>
     </aside>
   )
 }
