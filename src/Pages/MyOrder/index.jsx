a51089d33efdf269f9c47d9a4d62d5b9442b5851
Nico Mendoza <nicomendoza274@gmail.com>
2023-08-15 00:40:38 -0300

    Add orders list && view order

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,3 +1,5 @@
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+import { Link, useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import Layout from "../../Components/Layout";
@@ -6,15 +8,25 @@ import OrderCard from '../../Components/OrderCard';
 
 const MyOrder = () => {
   const context = useContext(ShoppingCartContext)
+  const params = useParams();
+  const indexOrderPath = Number(params.id)
+  const orders = !isNaN(indexOrderPath)  ? 
+    context.order?.[indexOrderPath]?.products : 
+    context.order?.slice(-1)[0]?.products
 
   return (
     <Layout>
-      MyOrder
+      <div className='flex items-center justify-center w-80 relative mb-6'>
+        <Link className='absolute left-0 ' to={'/my-orders'}>
+          <ChevronLeftIcon className='h-6 w-6 text-black cursor-pointer' />
+        </Link>
+        <h1>MyOrders</h1>
+      </div>
       <div className='flex flex-col w-80'>
         {
-          context.order?.slice(-1)[0].products.map((product) =>(
+          orders.map((product, index) =>(
             <OrderCard
-              id={product.id}
+              id={!isNaN(indexOrderPath) ? product.id : index}
               key={product.id}
               title={product.title} 
               imageUrl={product.images}
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,10 +1,26 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
+import OrdersCard from '../../Components/OrdersCard'
 import Layout from '../../Components/Layout'
 
 function MyOrders() {
-
+    const context = useContext(ShoppingCartContext)
+    console.log(context.order)
     return (
       <Layout>
-        MyOrders
+        <div className='flex items-center justify-center w-80 relative'>
+          <h1>MyOrders</h1>
+        </div>
+        {
+          context.order.map((order, index) => (
+            <Link key={index} to={`/my-orders/${index}`}>
+              <OrdersCard 
+                totalPrice={order.totalPrice } 
+                totalProducts={order.totalProducts}/>
+            </Link>
+            ))
+        }
       </Layout>
     )
   }
